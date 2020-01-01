ff7f0eb5364cb7859c40c926f9e8a3fabffa8339
Utkarsh Paliwal <utkarshpaliwal@github.com>
2020-01-01 12:27:32 +0530

    Game working fully
    Now need to work on making the website responsive

--- rgb.js
+++ rgb.js
@@ -1,7 +1,59 @@
+var rgbToHex = function (rgb) { 
+  var hex = Number(rgb).toString(16);
+  if (hex.length < 2) {
+       hex = "0" + hex;
+  }
+  return hex;
+};
+var fullColorHex = function(r,g,b) {   
+  var red = rgbToHex(r);
+  var green = rgbToHex(g);
+  var blue = rgbToHex(b);
+  return red+green+blue;
+};
+function generateColor(btn)
+{
+	var r = Math.floor(Math.random()*255)+1;
+	var g = Math.floor(Math.random()*255)+1;
+	var b = Math.floor(Math.random()*255)+1;
+	document.getElementById("color"+btn).style.backgroundColor = '#'+fullColorHex(r,g,b);
+}
 var r = Math.floor(Math.random()*255)+1;
 var g = Math.floor(Math.random()*255)+1;
 var b = Math.floor(Math.random()*255)+1;
 document.getElementById("color").innerHTML = "RGB("+ r+","+g+","+b+")";
+//document.getElementById("color").style.backgroundColor = '#'+fullColorHex(r,g,b);
+var ans_btn = Math.floor(Math.random()*6)+1;
+var ans = document.getElementById("color"+ans_btn);
+ans.style.backgroundColor = '#'+fullColorHex(r,g,b);
+for (var i = 1; i <= 6; i++) {
+	if(i==ans_btn)
+		continue;
+	generateColor(i);
+
+}
+function check(bt)
+{
+	if(bt[5] == ans_btn)
+	{
+		document.getElementById("result").innerHTML = "Yayy! Correct";
+		document.getElementById("jumbo").style.backgroundColor = '#'+fullColorHex(r,g,b);
+		for (var i = 1; i <= 6; i++) {
+			document.getElementById("color"+i).disabled = true;
+		}
+	}
+	else
+	{
+		document.getElementById("result").innerHTML = "Try Again";
+		var button = document.getElementById(bt);
+		button.style.backgroundColor = "transparent";
+		button.disabled = true;
+	}
+}
+
+
+
+
 var btn1 = document.getElementById("color1");
 var btn2 = document.getElementById("color2");
 var btn3 = document.getElementById("color3");
@@ -9,8 +61,3 @@ var btn4 = document.getElementById("color4");
 var btn5 = document.getElementById("color5");
 var btn6 = document.getElementById("color6");
 //bt1.style.backgroundColor: rgb(r,g,b);
-function check(bt)
-{
-	var b = document.getElementById(bt);
-	document.getElementById(bt).innerHTML = ":)";
-}
